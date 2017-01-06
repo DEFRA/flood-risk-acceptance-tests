d23313dc44b5aefcabc3f041586d79c58836a2bb
timstone <tim.stone@yahoo.co.uk>
2017-01-06 14:53:35 +0000

    added search for completed registration steps

--- features/step_definitions/back_office/edit_steps.rb
+++ features/step_definitions/back_office/edit_steps.rb
@@ -1,6 +1,6 @@
 When(/^I withdraw a submitted registration$/) do
-  @app.search_page.nav_bar.click_on	
-  @app.search_page.search_field.input '@exemption_number'
+  @app.search_page.nav_bar.home_link.click
+  @app.search_page.search_field.set @exemption_number
   @app.search_page.search_button.click
   @app.search_page.view_first_search_result.click
 end
